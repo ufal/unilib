234fd8d6441c58ae7f799ee40d9ba12a6b75e94f
Milan Straka <fox@ucw.cz>
2015-02-26 09:22:47 +0100

    Release version v3.0.0.

--- unilib/utf8.cpp
+++ unilib/utf8.cpp
@@ -7,7 +7,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-// UniLib version: 2.2.0-devel
+// UniLib version: 3.0.0
 // Unicode version: 7.0.0
 
 #include "utf8.h"
