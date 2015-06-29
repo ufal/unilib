dfd427822e3319eca99388ed72c41c2d448b1fd6
Milan Straka <fox@ucw.cz>
2015-06-29 18:25:00 +0200

    Release version 3.1.0.
    Most notable change is Unicode 8.0.0.

--- unilib/unicode.cpp
+++ unilib/unicode.cpp
@@ -7,7 +7,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-// UniLib version: 3.0.2-devel
+// UniLib version: 3.1.0
 // Unicode version: 8.0.0
 
 #include "unicode.h"
