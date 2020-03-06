d8276e70b7c11c677897f71030de7258cbb1f99e
Milan Straka <milan@strakovi.com>
2020-03-06 17:05:56 +0100

    Bump version to 3.2.1-devel.

--- unilib/utf8.cpp
+++ unilib/utf8.cpp
@@ -7,7 +7,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-// UniLib version: 3.2.0
+// UniLib version: 3.2.1-devel
 // Unicode version: 13.0.0
 
 #include "utf8.h"
