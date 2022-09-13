1281c7e384bc471d0f1e8f25fef33879626293fd
Milan Straka <milan@strakovi.com>
2022-09-13 17:43:59 +0200

    Regenerate sources for Unicode 15.0.0.
    The version got bumped to 3.3.0-dev, because updating Unicode data
    will cause at least a minor version bump.

--- unilib/utf16.cpp
+++ unilib/utf16.cpp
@@ -7,8 +7,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-// UniLib version: 3.2.1-devel
-// Unicode version: 13.0.0
+// UniLib version: 3.3.0-dev
+// Unicode version: 15.0.0
 
 #include "utf16.h"
 
