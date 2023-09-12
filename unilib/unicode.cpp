890feadfb0cc27bfcf5c9fc22d7e8c0cd0fd07c0
Milan Straka <milan@strakovi.com>
2023-09-12 11:56:49 +0200

    Bump version to 3.3.2-dev.

--- unilib/unicode.cpp
+++ unilib/unicode.cpp
@@ -7,7 +7,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-// UniLib version: 3.3.1
+// UniLib version: 3.3.2-dev
 // Unicode version: 15.0.0
 
 #include "unicode.h"
