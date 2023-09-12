d00b2b3ab77bf9f4a92be466112854370ec0c833
Milan Straka <milan@strakovi.com>
2023-09-12 11:53:05 +0200

    Stop using the std::iterator deprecated in C++17.

--- gen/template/utf16.h
+++ gen/template/utf16.h
@@ -112,8 +112,13 @@ void utf16::decode(const std::u16string& str, std::u32string& decoded) {
   decode(str.c_str(), decoded);
 }
 
-class utf16::string_decoder::iterator : public std::iterator<std::input_iterator_tag, char32_t> {
+class utf16::string_decoder::iterator {
  public:
+  using iterator_category = std::input_iterator_tag;
+  using value_type = char32_t;
+  using difference_type = ptrdiff_t;
+  using pointer = char32_t*;
+  using reference = char32_t&;
   iterator(const char16_t* str) : codepoint(0), next(str) { operator++(); }
   iterator(const iterator& it) : codepoint(it.codepoint), next(it.next) {}
   iterator& operator++() { if (next) { codepoint = decode(next); if (!codepoint) next = nullptr; } return *this; }
@@ -144,8 +149,13 @@ utf16::string_decoder utf16::decoder(const std::u16string& str) {
   return string_decoder(str.c_str());
 }
 
-class utf16::buffer_decoder::iterator : public std::iterator<std::input_iterator_tag, char32_t> {
+class utf16::buffer_decoder::iterator {
  public:
+  using iterator_category = std::input_iterator_tag;
+  using value_type = char32_t;
+  using difference_type = ptrdiff_t;
+  using pointer = char32_t*;
+  using reference = char32_t&;
   iterator(const char16_t* str, size_t len) : codepoint(0), next(str), len(len) { operator++(); }
   iterator(const iterator& it) : codepoint(it.codepoint), next(it.next), len(it.len) {}
   iterator& operator++() { if (!len) next = nullptr; if (next) codepoint = decode(next, len); return *this; }
