232f3cf18fb961c101659aeefa301f75b429f15d
Milan Straka <milan@strakovi.com>
2023-09-29 10:31:11 +0200

    Remove now unnecessary test_ucd_input.py.

--- tests/test_ucd_input.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("unicode_data_path", type=str, help="Path to UnicodeData file")
-args = parser.parse_args()
-
-with open(args.unicode_data_path, "r", encoding="ascii") as unicode_data_file:
-    print(unicode_data_file.read(), end="")
